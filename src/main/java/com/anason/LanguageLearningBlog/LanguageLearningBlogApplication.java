9b3885b555efdf36ceda1e464dddf4e70a3c9a6d
@revoltcode <oddybassey@gmail.com>
2021-01-26 04:04:58 +0100

    default project structure

---
+++ src/main/java/com/anason/LanguageLearningBlog/LanguageLearningBlogApplication.java
@@ -0,0 +1,13 @@
+package com.anason.LanguageLearningBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearningBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageLearningBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anason/LanguageLearningBlog/LanguageLearningBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anason.LanguageLearningBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageLearningBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
